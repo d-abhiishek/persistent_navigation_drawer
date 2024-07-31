33944b49c7a423b50d025b97c60005c3b215ce64
Abhishek Dhekane <abhishek.dhekane@xl2.de>
2024-07-31 10:26:33 +0200

    Remove unnecessary imports.

--- lib/favourites/favourites_d1.dart
+++ lib/favourites/favourites_d1.dart
@@ -1,4 +1,3 @@
-import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 
 class FavouritesPageDepth1 extends StatefulWidget {
--- lib/search/search_d1.dart
+++ lib/search/search_d1.dart
@@ -1,4 +1,3 @@
-import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 
 class SearchPageDepth1 extends StatefulWidget {
